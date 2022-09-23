fa37c85041f15ae425b47b4a6d1a95937ba9c033
Krittakorn Chairattanarom <6310682692@cn330>
2022-09-23 15:35:56 +0700

    debug main.py

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import csv
 # See https://fastapi.tiangolo.com/tutorial/first-steps/
 from fastapi import FastAPI
 
+# get country data from csv file  
 filename = "world_table_country.csv"
-with open(filename."r") as csv_file:
-    csv_reader = csv_reader(csv_file)
-    headers  = next(csv_reader)
+with open(filename, "r") as csv_file:
+    csv_reader = csv.reader(csv_file)
+    headers = next(csv_reader)
     data_country = [{k: v for (k, v) in zip(headers, row)} for row in csv_reader]
 
 # create an instance of class FastAPI named "app"
@@ -18,6 +19,9 @@ app = FastAPI()
 async def read_root() -> dict:
     return {"message": "Hello there"}
 
+
+# define function that handles "GET" request with endpoint "/world"
+# list all countries
 @app.get("/world")
 async def read_countries() -> dict:
     return {"result": data_country}
